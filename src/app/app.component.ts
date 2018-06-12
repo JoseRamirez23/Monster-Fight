6931002724d2e592460ff67a6676c13eea893c7a
Jose Ramirez <shadow661199@gmail.com>
2018-06-12 16:11:01 -0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
   monsterName:string = 'kaiju';
+  monsterPower: number;
+
+  monster:any[]=[
+    {
+
+      monsterName:'kaiju',
+      monsterPower: null
+    },
+    {
+
+      monsterName:'smaug',
+      monsterPower:null
+    }
+    
+  ];
+  monsterSelected;
+  ngOnInit(){
+
+
+  }
+  pickMonster(){
+    const index = Math.floor(Math.random()* this.monster.length);
+    this.monsterSelected = this.monster[index];
+  }
+
+  generatePower(){
+   this.monsterSelected.monsterPower = Math.floor(Math.random()*1000);
+
+  }
 }
